f20a59f22f0fa56c11614f72ed20a1838b0570f2
Michiel Cuijpers <m.cuijpers@sig.eu>
2019-11-12 10:46:18 +0100

    Create sol2.sol

---
+++ sol2.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.4.0 <0.7.0;
+
+contract SimpleStorage {
+
+    uint8 storedData;
+
+    function set(uint8 x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint8) {
+        return storedData;
+    }
+
+}
